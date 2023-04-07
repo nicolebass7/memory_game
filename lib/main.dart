2eaf2617acf1bed638c14c7994d9663817d38612
trinajkenyon <trina.kenyon@eagles.oc.edu>
2023-04-06 20:44:41 -0500

    Created a first draft of a home page, which includes method stubs for both buttons. Changed the main.dart to load the homepage, which needs changed once there's a login page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:memory_game/pages/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
@@ -100,7 +101,6 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
