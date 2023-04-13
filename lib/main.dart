a37565722d8f58812bc9e3ba4295d3dd04e8da98
trinajkenyon <trina.kenyon@eagles.oc.edu>
2023-04-13 13:23:57 -0500

    Added firebase firestore to the project. I haven't written anything that actually interacts with the database yet, but it exists.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:memory_game/pages/homepage.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
+
 }
 //hello
 class MyApp extends StatelessWidget {
@@ -18,7 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      localizationsDelegates: [
+      localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
