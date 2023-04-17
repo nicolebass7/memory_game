04526a221d68c6562561d3d2b81d5cba280c0701
nicole <nicole.bass@eagles.oc.edu>
2023-04-17 13:40:49 -0500

    made game page

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,204 @@
+// import 'package:flipcard/homepage.dart';
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatefulWidget {
+//   // This widget is the root of your application.
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         // This is the theme of your application.
+//         //
+//         // Try running your application with "flutter run". You'll see the
+//         // application has a blue toolbar. Then, without quitting the app, try
+//         // changing the primarySwatch below to Colors.green and then invoke
+//         // "hot reload" (press "r" in the console where you ran "flutter run",
+//         // or simply save your changes to "hot reload" in a Flutter IDE).
+//         // Notice that the counter didn't reset back to zero; the application
+//         // is not restarted.
+//         primarySwatch: Colors.blue,
+//         // This makes the visual density adapt to the platform that you run
+//         // the app on. For desktop platforms, the controls will be smaller and
+//         // closer together (more dense) than on mobile platforms.
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:memory_game/pages/game_screen.dart';
+import 'package:memory_game/pages/info_card.dart';
 
 void main() {
   runApp(const MyApp());
 }
-//hello
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: GameScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+// class GameScreen extends StatefulWidget {
+//   const GameScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   _GameScreenState createState() => _GameScreenState();
+// }
+//
+// class _GameScreenState extends State<GameScreen> {
+//   int addSeconds = 0;
+//   late Timer timer;
+//   void addSecond() {
+//     setState(() {
+//       addSeconds++;
+//     });
+//   }
+//
+//   void startTimer() {
+//      timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+//        print('tick');
+//       addSecond();
+//     });
+//   }
+//
+//   TextStyle whiteText = const TextStyle(color: Colors.white);
+//   bool hideTest = false;
+//   Game _game = Game();
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _game.initGame();
+//     startTimer();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final shownMinutes = '0';
+//     final shownSeconds = '$addSeconds';
+//
+//
+//     return Scaffold(
+//       backgroundColor: const Color(0xFFE55870),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: [
+//           const Center(
+//             child: Text(
+//               "Memory Game",
+//               style: TextStyle(
+//                 fontSize: 48.0,
+//                 fontWeight: FontWeight.bold,
+//                 color: Colors.white,
+//               ),
+//             ),
+//           ),
+//
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceAround,
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//           ),
+//
+//           // SizedBox(
+//           //height: 800,
+//           //width: 800,
+//           // height: MediaQuery.of(context).size.width,
+//           //width: MediaQuery.of(context).size.width,
+//
+//           Center(
+//             child: GridView.builder(
+//                 itemCount: _game.gameImg!.length,
+//                 shrinkWrap: true,
+//                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//                   crossAxisCount: 3,
+//                   crossAxisSpacing: 16.0,
+//                   mainAxisSpacing: 16.0,
+//                 ),
+//                 padding: const EdgeInsets.all(16.0),
+//                 itemBuilder: (context, index) {
+//                   return GestureDetector(
+//                     onTap: () {
+//                       print(_game.card_list[index]);
+//                       setState(() {
+//                         _game.gameImg![index] = _game.card_list[index];
+//                         _game.matchCheck.add({index: _game.card_list[index]});
+//                       });
+//
+//                       if (_game.matchCheck.length == 2) {
+//                         if (_game.matchCheck[0].values.first ==
+//                             _game.matchCheck[1].values.first) {
+//                           print("true");
+//
+//                           _game.matchCheck.clear();
+//                           if (!_game.gameImg!.any((img) => img == Game.cardPath)) {
+//                             // stop
+//                                     timer.cancel();
+//                           }
+//                         } else {
+//                           print(false);
+//                           Future.delayed(const Duration(milliseconds: 500), () {
+//                             setState(() {
+//                               _game.gameImg![_game.matchCheck[0].keys.first] =
+//                                   Game.cardPath;
+//                               _game.gameImg![_game.matchCheck[1].keys.first] =
+//                                   Game.cardPath;
+//
+//                               _game.matchCheck.clear();
+//                             });
+//                           });
+//                         }
+//                       }
+//                     },
+//                     child: Container(
+//                       padding: const EdgeInsets.all(16.0),
+//                       decoration: BoxDecoration(
+//                         color: const Color(0xFFFFB46A),
+//                         borderRadius: BorderRadius.circular(8.0),
+//                         image: DecorationImage(
+//                           image: AssetImage(_game.gameImg![index]),
+//                           fit: BoxFit.cover,
+//                         ),
+//                       ),
+//                     ),
+//                   );
+//                 }),
+//           ),
+//
+//           Center(
+//             child: Text(
+//               "$shownMinutes:$shownSeconds",
+//               style: const TextStyle(
+//                 fontSize: 48.0,
+//                 fontWeight: FontWeight.bold,
+//                 color: Colors.white,
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
