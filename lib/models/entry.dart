b9862c7775f103613faf4772ce9da2a829ac3641
trinajkenyon <trina.kenyon@eagles.oc.edu>
2023-04-18 12:59:24 -0500

    Cleaned up, should work now

--- lib/models/entry.dart
+++ lib/models/entry.dart
@@ -1,9 +1,9 @@
 
 class Entry {
-  Entry({this.user = '', required this.time});
-
+  Entry({this.user = '', required this.time, required this.id});
   final String user;
   final int time;
+  final String id;
 
   Map<String, dynamic> toMap() {
     return {
